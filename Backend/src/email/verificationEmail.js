ec1dc8944c705840950375837157216543ff0a92
asifandhi <asifandhi267@gmail.com>
2026-06-12 22:31:29 +0530

    version 2.2

--- Backend/src/email/verificationEmail.js
+++ Backend/src/email/verificationEmail.js
@@ -5,7 +5,7 @@ export const verificationEmailHTML = (username,otp)=> `
     <h2>Hey ${username}, verify your account</h2>
     <p>Your verification code is:</p>
     <h1 style="letter-spacing: 8px; color: #4F46E5;">${otp}</h1>
-    <p>This code expires in <strong>1 hour</strong>.</p>
+    <p>This code expires in <strong>half hour</strong>.</p>
     <p>If you didn't sign up, ignore this email.</p>
   </body>
 </html>
